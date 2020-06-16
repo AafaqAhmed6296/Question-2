acd232e08c6c6a1d830a5d09bf0dd3f9fd68d50b
Afaq Ahmed Saqi <afaqahmad6296@gmail.com>
2020-06-16 16:48:37 +0500

    get put function defined.

--- Question#2/main.cpp
+++ Question#2/main.cpp
@@ -19,11 +19,37 @@ class Library {
     int daysleft;
     static int totalBooksLeft;
 public:
+    Library();
     void issueBook();
     void getData();
+    void putData();
     void booksLeft();
     void remainingDays();
 };
+
+int Library:: totalBooksLeft = 0.0;
+
+Library::Library()
+:publishedYear(0),copies(0),issueDate(0),daysleft(0){
+    
+}
+
+void Library::getData() {
+    cout << "Enter book name "; cin >> bookName;
+    cout << "Enter author name "; cin >> authorName;
+    cout << "Enter publisher name: "; cin >> publisherName;
+    cout << "Enter published year: "; cin >> publishedYear;
+    cout << "Enter no. of copies: "; cin >>copies;
+}
+
+void Library::putData() {
+    cout << "Book Name: " << bookName;
+    cout << "Author name: " << authorName;
+    cout << "Publisher name: " << publisherName;
+    cout << "Published Year: " << publishedYear;
+    cout << "No. of copies: " << copies;
+}
+
 
 int main(){
     
