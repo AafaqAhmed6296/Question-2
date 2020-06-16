520f31cabaa9c4683363085506f418ef8a3a5c11
Afaq Ahmed Saqi <afaqahmad6296@gmail.com>
2020-06-16 16:15:30 +0500

    update gitignore

---
+++ Question#2/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Question#2
+//
+//  Created by Afaq Ahmed on 16/06/2020.
+//  Copyright © 2020 Afaq Ahmed. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+class Library {
+    char bookName[20];
+    char authorName[20];
+    
+};
+
+int main(){
+    
+    return 0;
+}
